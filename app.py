ff2c80602917490be42096e704dbc8aecdcc0d5e
Kenta Xu <43323628+larm60@users.noreply.github.com>
2022-10-22 20:22:51 -0400

    Add files via upload
    final

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from email.headerregistry import AddressHeader
 from pyexpat import model
 from flask import Flask, render_template,request,jsonify,url_for
 import pandas as pd
@@ -10,18 +11,19 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 import pickle
-
+import bcrypt
 import requests
 import json
 
+
 app = Flask(__name__)
 
-# from google.cloud import firestore
-# from google.cloud import bigquery
+from google.cloud import firestore
+from google.cloud import bigquery
 
-#client = bigquery.Client()
+client = bigquery.Client()
 
-#db = firestore.Client() #for database
+db = firestore.Client() #for database
 
 
 @app.route("/")
@@ -29,50 +31,101 @@ def home():
     return render_template('landingpage.html')
 
 @app.route("/register",methods=["GET","POST"])
-def model_training():
+def create_profile():
 
-    # if request.method == "POST":
-    #     fname = request.form.get("fname")
-    #     lname = request.form.get("lname")
-    #     uname = request.form.get("uname")
+    if request.method == "POST":
+        fname = request.form.get("fname")
+        lname = request.form.get("lname")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        password2 = request.form.get("password2")
+        sex = request.form.get("sex")
+        dob = request.form.get("DOB")
+        other = request.form.get("other")
+        street = request.form.get("street")
+        
+        doc_ref = db.collection('users').document(email)
+        doc = doc_ref.get()
+        if doc.exists:
+            return render_template('register.html') + "<p align = 'center'>Email already exists</p>"
+        else:
+            if password == password2:
+                hashed = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt()) #encrypts the password
 
-    #     doc_ref = db.collection(u'users').document(uname)
-    #     doc_ref.set({
-    #         u'uname' : uname,
-    #         u'first': fname,
-    #         u'last': lname
-    #     })
+                doc_ref = db.collection(u'users').document(email)
+                doc_ref.set({
+                    u'email' : email,
+                    u'first': fname,
+                    u'last': lname,
+                    u'password' : hashed,
+                    u'sex' : sex,
+                    u'dob': dob,
+                    u'other': other,
+                    u'street': street
+                })
+
+                return render_template('enter_symptoms.html')
+            else:
+                return render_template('register.html') + "<p align = 'center'>Passwords don't match</p>"
 
     return render_template('register.html')
 
-
-@app.route("/predict",methods=["GET","POST"])
-def predict_entries():
-
+@app.route("/enter_symptoms",methods=["GET","POST"])
+def add_symptoms():
     if request.method == "POST":
-       
-        RM =  request.form.get("rm")
-        DIS = request.form.get("dis")
-        
-        # load model
-        with open('model.pkl', 'rb') as f:
-            model = pickle.load(f)
+        temp = request.form.getlist("symptoms")
+        response = requests.get("http://api.endlessmedical.com/v1/dx/InitSession")
+        id = response.json()['SessionID']
 
-        prediction = model.predict(np.array([[RM,DIS]]))
-        #predicts = ' $ '+str(int(prediction)*10000)
-        predicts = ' $ '+ str(int((round(float(prediction),5))*10000))
-        prediction = float(prediction)
+        value = "1"
+
+        response = requests.post("http://api.endlessmedical.com/v1/dx/AcceptTermsOfUse?SessionID=" + id + "&passphrase=" + "I have read, understood and I accept and agree to comply with the Terms of Use of EndlessMedicalAPI and Endless Medical services. The Terms of Use are available on endlessmedical.com")
+
+        for name in temp:
+            response = requests.post("http://api.endlessmedical.com/v1/dx/UpdateFeature?SessionID="+id+"&name=" + name + "&value=" + value)
+
+        response = requests.get("http://api.endlessmedical.com/v1/dx/Analyze?SessionID=" + id)
+
+        diseases = response.json()['Diseases']
+
+        output = ""
+        query = ""
+        if diseases == []:
+            output = "<p>Healthy</p>"
+            query = 'SELECT Title, URL FROM hackathon-366314.hackathon.clinicals WHERE Upper(Conditions) LIKE Upper("Healthy") LIMIT 5'
+
+        #STATUS = "Available" AND
         
-        with sqlite3.connect("Realty_Listings.db") as conn:
-            c = conn.cursor()
-            sql = ("INSERT INTO Housing_Price (RM, DIS, Price_10000)" "VALUES (?, ?, ?)")
-            val = (RM, DIS, prediction)
-            c.execute(sql, val)
+        else:
+
+            query = 'SELECT Title, URL FROM hackathon-366314.hackathon.clinicals WHERE STATUS = "Available" '  #used to access GCP BigQuery to search clinicals dataset
+
+            output = "<p align = 'center'>Possible Diseases: <br>"
+            for d in diseases:
+                for i in d.keys():
+                    output += i  #adds disease to print in html page
+                    output += "   ;    "
+                    query += 'AND Upper(Conditions) LIKE Upper("%' + i + '%") '
+            output += "</p>"
+
+            query += "LIMIT 5"
+        
+        results = client.query(query)
             
-        #return predicts
-        return render_template("output.html",RM=RM,DIS=DIS,predicts=predicts)
-    
-    return render_template('predict.html')
+        clinicals = "<p>Clinical Trials: <br> "
+
+        for row in results:
+            title = row['Title']
+            url = row['URL']
+            clinicals += title + " : " + url
+            clinicals += "<br>"
+
+        clinicals += "</p>"
+
+        return render_template('enter_symptoms.html') + output + clinicals
+
+
+    return render_template('enter_symptoms.html')
 
 
 app.run(debug=True)
